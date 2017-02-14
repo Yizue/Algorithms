4edb07a732cb9f9c2d1b782523c88f93eee13fe1
Steve Hirabayashi <Yizue14@gmail.com>
2017-02-14 15:28:17 -0500

    Update MaximumFlow.cpp

--- MaximumFlow.cpp
+++ MaximumFlow.cpp
@@ -2,76 +2,115 @@
 using namespace std;
 
 const int INF = 1.0e9;
-const int NEG_INF = -1.0e9;
 
 #define MEM(arr,val)memset(arr,val, sizeof arr)
-#define forn(i,n) for(int (i) = 0; (i) < (n); (i)++)
-#define fornm(i,n,m) for(int (i) = (n); (i) < (m); (i)++)
-#define formn(i,m,n) for(int (i) = (m); (i) > (n); (i)--)
-#define DEBUG if(debugging)
+#define forn(i,n) for(int (i) = 0; (i) < (n); ++(i))
+#define fornm(i,n,m) for(int (i) = (n); (i) < (m); ++(i))
+#define formn(i,m,n) for(int (i) = (m); (i) > (n); --(i))
 
 #define FASTER ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
+typedef long long ll;
 typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
 
-const int debugging = 1;
+int gcd(int a,int b){return b == 0 ? a : gcd(b,a%b);}
+int lcm(int a,int b){return a*(b/gcd(a,b));}
 
-const int MAX_V = 10;
-int s, t, mF, pF; // s = src, t = sink, mF = maxFlow, pF = minPathFlow
-vector<vii> G; // Graph using Adj List
-int rG[MAX_V][MAX_V]; // Residual Graph using Adj Matrix, stores weight of edge (row -> col)
-vi p; // Path found by BFS
+//----------------------------------------------------------------------//
 
-void augment(int v, int minEdge) {     // traverse BFS spanning tree from s to t
-  if (v == s) { pF = minEdge; return; }  // record minEdge in a global variable f
-  else if (p[v] != -1) { augment(p[v], min(minEdge, rG[p[v]][v])); // recursive
-                         rG[p[v]][v] -= pF; rG[v][p[v]] += pF; }       // update
+class Edge{
+    public:
+        int u,v,cap,f;
+
+        Edge(int _u, int _v, int _cap) : u(_u),v(_v),cap(_cap){ f = 0; }
+
+        int other(int vertex){
+            if(vertex == u)return v;
+            else if(vertex == v)return u;
+            return -1;
+        }
+        int residualTo(int vertex){
+            if(vertex == u)return f;
+            return cap - f;
+        }
+        void addFlowTo(int vertex, int ff){
+            if(vertex == u) f -= ff;
+            else if(vertex == v) f += ff;
+        }
+};
+
+typedef vector<Edge*> edgeList;
+
+#define MAX_V 1000
+#define MAX_E 1000
+const int SRC = 0;
+const int SINK = 1;
+int flow;
+edgeList parent;
+vi visited;
+vector<edgeList> G;
+
+void addEdge(int u, int v, int cap){
+	Edge* e = new Edge(u,v,cap);
+	G[u].push_back(e); // Pass same edge object to u and v lists
+	G[v].push_back(e);
 }
 
-// O(VE^2) Edmonds-Karp's algorithm
-void edmondsKarp() {
-    mF = 0;
-    while (1) {
-        pF = 0;
-        bitset<MAX_V> vis; vis[s] = true; // we change vi dist to bitset!
-        queue<int> q; q.push(s); // BFS portion of the code
-        p.assign(MAX_V, -1);
-        while (!q.empty()) {
-            int u = q.front(); q.pop();
-            if (u == t) break;
-            forn (j, (int)G[u].size()) {
-                int v = G[u][j].first;
-                if (rG[u][v] > 0 && !vis[v])
-                    vis[v] = true, q.push(v), p[v] = u;
-        }}
-        augment(t, INF); // find the min edge weight `f' along this path, if any
-        if (pF == 0) break; // we cannot send any more flow (`f' = 0), terminate
-        mF += pF;   // we can still send a flow, increase the max flow!
-    }
+bool augmentingPath(){
+	queue<int> q; q.push(SRC);
+	forn (i, MAX_V) parent.push_back(NULL);
+	visited.assign(MAX_V, 0); visited[SRC] = 1;
+	while(!q.empty()){ // BFS - find existing path and store it
+		int u = q.front(); q.pop();
+		if(u == SINK) break;
+		forn (i, (int)G[u].size()) {
+			Edge* e = G[u][i];
+			int v = e->other(u);
+			if(!visited[v] && e->residualTo(v) > 0){
+				visited[v]=1;
+				parent[v]=e;
+				q.push(v);
+			}
+		}
+	}
+	return visited[SINK]; // Found path to the Sink
 }
 
-int main() {
-	FASTER;
-	// Initialize and populate G and rG
-	int V = 7; s = 0; t = 6;
-    memset(rG, 0, sizeof rG);
-    G.assign(V, vii());
-    G[0].push_back(ii(1, 25)); rG[0][1] = 25;
-    G[0].push_back(ii(2, 9)); rG[0][2] = 9;
-    G[1].push_back(ii(0, 25)); rG[1][0] = 25;
-    G[1].push_back(ii(3, 13)); rG[1][3] = 13;
+// Edmonds-Karp Algorithm O(V^2 E)
+int maxFlow(){
+	int mf = 0;
+	while(augmentingPath()){
+		flow = INF;
+		for(int v=SINK; v!=SRC ; v=parent[v]->other(v)){ // Find min flow in path
+			flow = min(flow, parent[v]->residualTo(v));
+		}
+		for(int v=SINK; v!=SRC ; v=parent[v]->other(v)){
+			parent[v]->addFlowTo(v,flow);
+		}
+		mf+=flow;
+	}
+	return mf;
+}
 
-    G[2].push_back(ii(0, 9));  rG[2][0] = 9;
-    G[2].push_back(ii(3, 17)); rG[2][3] = 17;
 
-    G[3].push_back(ii(2, 9));  rG[3][2] = 9;
-    G[3].push_back(ii(5, 20)); rG[3][5] = 20;
-    G[4].push_back(ii(3, 7));  rG[4][3] = 7;
-    G[4].push_back(ii(5, 4));  rG[4][5] = 4;
+void data1();
 
-    edmondsKarp(); printf("Maxflow from %d to %d: %d\n", s, t, mF); // 18
+int main(){
+    FASTER;
 
-	return 0;
+    G.assign(MAX_V, edgeList());
+	addEdge(SRC,2,2);
+	addEdge(SRC,3,3);
+	addEdge(2,4,5);
+	addEdge(2,5,1);
+	addEdge(4,SINK,1);
+	addEdge(5,SINK,1);
+	addEdge(3,SINK,2);
+    assert(maxFlow() == 4);
+    printf("All test passed\n");
+
+    return 0;
 }
